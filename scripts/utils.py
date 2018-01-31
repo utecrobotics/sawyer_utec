624bbc9a0e963aedb81eb17bf0057f46c2c32405
Oscar E. Ramos <15218582+oscar-ramos@users.noreply.github.com>
2018-01-31 01:28:22 -0500

    Add quaternion multiplication and comments

--- scripts/utils.py
+++ scripts/utils.py
@@ -8,8 +8,14 @@ import numpy as np
 def quaternionFromAxisAngle(angle, axis):
     """
     Increment in pose with respect to the global frame.
-        angle - double [deg]
-        axis - (x,y,z) if not unitary, it will be internally normalized
+
+    Arguments:
+        angle -- double [deg]
+        axis -- (x,y,z) if not unitary, it will be internally normalized
+
+    Output:
+      quaternion in the format (ew, ex, ey, ez) where ew is the scalar part
+
     """
     angle2 = np.deg2rad(angle)/2.0
     ax = np.array([axis]).transpose()
@@ -21,8 +27,17 @@ def quaternionFromAxisAngle(angle, axis):
     return Q
 
 
-def rotationToQuaternion(R):
+def quaternionFromRotation(R):
+    """
+    Convert a rotation matrix to a unitary quaternion.
+    
+    Arguments:
+      R -- rotation matrix as a 2d numpy array
 
+    Output:
+      quaternion in the format (ew, ex, ey, ez) where ew is the scalar part
+
+    """
     dEpsilon = 1e-6
     quat = [0., 0., 0., 0.]
 
@@ -43,7 +58,32 @@ def rotationToQuaternion(R):
     return quat
 
 
+def quaternionMult(q1, q2):
+    """
+    Multiply 2 quaternions. Each quaternion format is (ew, ex, ey, ez) where
+    ex is the scalar part.
+
+    Arguments:
+      q1 -- First quaternion
+      q2 -- Second quaternion
+
+    Return:
+      The quaternion multiplication (q1*q2)
+
+    """
+    res = [0., 0., 0., 0.]
+    res[0] = -q1[1]*q2[1] - q1[2]*q2[2] - q1[3]*q2[3] + q1[0]*q2[0]
+    res[1] =  q1[0]*q2[1] - q1[3]*q2[2] + q1[2]*q2[3] + q1[1]*q2[0]
+    res[2] =  q1[3]*q2[1] + q1[0]*q2[2] - q1[1]*q2[3] + q1[2]*q2[0]
+    res[3] = -q1[2]*q2[1] + q1[1]*q2[2] + q1[0]*q2[3] + q1[3]*q2[0]
+    return res
+
+
 def sgn(x):
+    """
+    Sign function 
+
+    """
     if (x >= 0):
         return 1.0
     else:
